72dd4dca3ec357ac0b1ed18fc0869699038a44cf
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-07-20 15:26:40 +0530

    UPGRADE CONTRACT

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,17 @@
+// import {ethers , upgrades} from "hardhat"
 const {ethers, upgrades} = require("hardhat")
 
 async function main(){
   const numContract = await ethers.getContractFactory("Num")
-  console.log("Deploying the Contract Version 1");
-  const num = await upgrades.deployProxy(numContract,[10],{
-    initializer :"valuesetter"
+  // console.log("Deploying the Contract Version 1");
+  const first  = await numContract.deploy();
+  console.log(first.target);
+  const num = await upgrades.deployProxy(numContract, [10], {
+    initializer : "setvalue"
   })
-  await num.deployed()
-  console.log("Num 1 deployed address", num.address);
+  await num.waitForDeployment();
+  console.log("Num 1 deployed address", num.getAddress());
+  
 }
 
 main();
